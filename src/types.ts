d2b451300adf2a5971c2fb5f0f6d0088d826bac9
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-12-01 18:47:46 +0900

    feat: add VPM specific fields

--- src/types.ts
+++ src/types.ts
@@ -14,18 +14,26 @@ export type OutputType = {
 export const PackageManifestSchema = v.object({
   name: v.string(),
   version: v.string(),
-  displayName: v.optional(v.string()),
+  displayName: v.string(),
+  url: v.string(),
+  vpmDependencies: v.record(v.string(), v.string()),
+  author: v.object({
+    name: v.string(),
+    email: v.string(),
+    url: v.optional(v.string()),
+  }),
+
   description: v.optional(v.string()),
   unity: v.optional(v.string()),
   unityRelease: v.optional(v.string()),
   dependencies: v.optional(v.record(v.string(), v.string())),
   keywords: v.optional(v.array(v.string())),
   type: v.optional(v.string()),
-  author: v.optional(v.object({
-    name: v.string(),
-    email: v.optional(v.string()),
-    url: v.optional(v.string()),
-  })),
+  legacyFolders: v.optional(v.string()),
+  legacyFiles: v.optional(v.string()),
+  legacyPackages: v.optional(v.string()),
+  zipSHA256: v.optional(v.string()),
+  changeLogUrl: v.optional(v.string()),
 });
 
 export const RepositoryManifestSchema = v.object({
