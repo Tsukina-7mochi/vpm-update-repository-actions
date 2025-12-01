a4a93742f5b294733f80e000deca615b5a133896
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-12-01 18:48:19 +0900

    build: update build configuration

--- build.ts
+++ build.ts
@@ -5,9 +5,11 @@ await esbuild.build({
   entryPoints: ["src/main.ts"],
   outfile: "dist/index.js",
   platform: "node",
+  target: "node20",
   bundle: true,
   minify: true,
   treeShaking: true,
+  sourcemap: "inline",
   plugins: [denoPlugin()],
 });
 
